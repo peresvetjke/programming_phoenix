cd6881e35c583d85761dfaeb3c158724f218e764
Udalov Igor <i.udalov@rambler-co.ru>
2023-04-09 16:37:44 +0300

    Add sockets and channel

--- assets/js/app.js
+++ assets/js/app.js
@@ -39,12 +39,7 @@ liveSocket.connect()
 // >> liveSocket.disableLatencySim()
 window.liveSocket = liveSocket
 
-// import player
-import Player from "./player"
-let video = document.getElementById("video")
+import socket from "./socket"
+import Video from "./video"
 
-if (video) {
-  Player.init(video.id, video.getAttribute("data-player-id"), () => {
-    console.log("player ready!")
-  })
-}
+Video.init(socket, document.getElementById("video"))
