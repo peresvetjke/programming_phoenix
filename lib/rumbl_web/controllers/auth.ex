102d7f366e93da1278848de7ef3e489be0a5c366
Udalov Igor <i.udalov@rambler-co.ru>
2023-04-09 08:47:18 +0300

    Fix router helpers import

--- lib/rumbl_web/controllers/auth.ex
+++ lib/rumbl_web/controllers/auth.ex
@@ -2,7 +2,6 @@ defmodule Rumbl.Auth do
   import Plug.Conn
   import Phoenix.Controller, only: [put_flash: 3, redirect: 2]
   import Comeonin.Bcrypt, only: [checkpw: 2, dummy_checkpw: 0]
-  import RumblWeb.Router.Helpers
 
   def init(opts) do
     opts
@@ -30,7 +29,7 @@ defmodule Rumbl.Auth do
     else
       conn
       |> put_flash(:error, "You must be logged in to access that page")
-      |> redirect(to: page_path(conn, :index))
+      |> redirect(to: RumblWeb.Router.Helpers.page_path(conn, :index))
       |> halt()
     end
   end
