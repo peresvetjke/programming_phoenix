cfe0ccbd96a30c2fa92397c987694489648d5348
Udalov Igor <i.udalov@rambler-co.ru>
2023-04-09 13:27:07 +0300

    Add slugs and protocol to parse id from them

--- lib/rumbl/rumbl_video/video.ex
+++ lib/rumbl/rumbl_video/video.ex
@@ -5,12 +5,15 @@ defmodule Rumbl.RumblVideo.Video do
   @required_fields ~w(url title description)a
   @optional_fields ~w(category_id)a
 
+  @primary_key {:id, Rumbl.Permalink, autogenerate: true}
   schema "videos" do
     field(:description, :string)
     field(:title, :string)
     field(:url, :string)
+    field(:slug, :string)
+
     belongs_to(:user, Rumbl.User)
-    belongs_to :category, Rumbl.Category
+    belongs_to(:category, Rumbl.Category)
 
     timestamps()
   end
@@ -20,6 +23,27 @@ defmodule Rumbl.RumblVideo.Video do
     video
     |> cast(attrs, @required_fields ++ @optional_fields)
     |> validate_required(@required_fields)
+    |> slugify_title()
     |> assoc_constraint(:category)
+  end
+
+  defp slugify_title(changeset) do
+    if title = get_change(changeset, :title) do
+      put_change(changeset, :slug, slugify(title))
+    else
+      changeset
+    end
+  end
+
+  defp slugify(str) do
+    str
+    |> String.downcase()
+    |> String.replace(~r/[^\w-]+/u, "-")
+  end
+
+  defimpl Phoenix.Param, for: Rumbl.RumblVideo.Video do
+    def to_param(%{slug: slug, id: id}) do
+      "#{id}-#{slug}"
+    end
   end
 end
